2a0a4021e8175b2e5afbce566e12082de9f74cf9
Mahir Singh Pawar <mahirsinghpawar25@gmail.com>
2026-01-19 23:35:20 +0530

    Day 1: Initial project setup and app skeleton

---
+++ app/src/main/java/com/dyl/drawyourlift/ui/home/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.dyl.drawyourlift.ui.home;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.dyl.drawyourlift.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
